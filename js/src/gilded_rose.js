ac87c10f7c373bda6aa8d268b6b651bf62d47b6f
Damien Shaw <damien.shaw@hotmail.co.uk>
2020-09-20 22:29:28 +0100

    refactor: changed updateValues function

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -11,45 +11,39 @@ function update_quality() {
   const BACKSTAGE_PASSES = 'Backstage passes';
   const CONJURED = 'Conjured';
   const SULFURAS = 'Sulfuras';
-  // const ITEM = 'Item'
+  const ITEM = 'Item'
 
-  const getUpdateValues = (item) => {
-    const sulfurasRegex = new RegExp('^' + SULFURAS, "g"); // originally used a template string but that looked confusing
+  const updateValues = (item) => ({
+    [SULFURAS]: { sellInUpdateValue: 0 },
+    [CONJURED]: { qualityUpdateValue: -2 },
+    [AGED_BRIE]: { qualityUpdateValue: 1 },
+    [BACKSTAGE_PASSES]: {
+      qualityUpdateValue:
+        item.sell_in < 0 ? -item.quality
+        : item.sell_in > 10 ? 1
+        : item.sell_in > 6 ? 2
+        : 3
+    },
+    [ITEM]: { qualityUpdateValue: item.sell_in === 0 ? -2 : -1 }
+  })
+
+  const getUpdateValues = (item, updateValues) => {
+    const { name } = item;
+    const sulfurasRegex = new RegExp('^' + SULFURAS, "g");
     const backstageRegex = new RegExp('^' + BACKSTAGE_PASSES, "g");
     const conjuredRegex = new RegExp('^' + CONJURED, 'g');
 
-    const sulfurasValues = { sellInUpdateValue: 0 }
-    const conjuredValues = { qualityUpdateValue: -2 }
-    const ageBrieValues = { qualityUpdateValue: 1 }
-    const backstageValues = (() => {
+    const type =
+      sulfurasRegex.test(name) ? SULFURAS
+      : conjuredRegex.test(name) ? CONJURED
+      : name === AGED_BRIE ? AGED_BRIE
+      : backstageRegex.test(name) ? BACKSTAGE_PASSES
+      : ITEM
 
-      const qualityUpdateValue =
-      item.sell_in < 0 ? -item.quality
-      : item.sell_in > 10 ? 1
-      : item.sell_in > 6 ? 2
-      : 3;
-
-      return { qualityUpdateValue }
-    })()
-    const itemValues = (() => {
-      const qualityUpdateValue =
-        item.sell_in === 0 ? -2 : -1;
-
-      return { qualityUpdateValue }
-    })()
-
-    const updateValues =
-      sulfurasRegex.test(item.name) ? sulfurasValues
-      : conjuredRegex.test(item.name) ? conjuredValues
-      : item.name === AGED_BRIE ? ageBrieValues
-      : backstageRegex.test(item.name) ? backstageValues
-      : itemValues
-
-    return updateValues;
+    return updateValues[type];
   }
 
   const setQuailty = (item, upgradeValues) => {
-
     const MAX_QUALITY = 50;
     const MIN_QUALITY = 0;
 
@@ -69,7 +63,7 @@ function update_quality() {
   }
 
   items.forEach(item => {
-    const updateValue = getUpdateValues(item);
+    const updateValue = getUpdateValues(item, updateValues(item));
 
     setQuailty(item, updateValue);
     setSellIn(item, updateValue);
