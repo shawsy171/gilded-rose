4a59f9028af530760a0281226eb14d6656b8abf2
Damien Shaw <damien.shaw@hotmail.co.uk>
2020-09-20 20:18:14 +0100

    refactor: named the update values

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -14,33 +14,38 @@ function update_quality() {
   // const ITEM = 'Item'
 
   const getUpdateValues = (item) => {
-
     const sulfurasRegex = new RegExp('^' + SULFURAS, "g"); // originally used a template string but that looked confusing
     const backstageRegex = new RegExp('^' + BACKSTAGE_PASSES, "g");
     const conjuredRegex = new RegExp('^' + CONJURED, 'g');
 
-    const updateValuesFn =
-      sulfurasRegex.test(item.name) ? () => ({ sellInUpdateValue: 0 })
-      : conjuredRegex.test(item.name) ? () => ({ qualityUpdateValue: -2 })
-      : item.name === AGED_BRIE ? () => ({ qualityUpdateValue: 1 })
-      : backstageRegex.test(item.name) ? () => {
+    const sulfurasValues = { sellInUpdateValue: 0 }
+    const conjuredValues = { qualityUpdateValue: -2 }
+    const ageBrieValues = { qualityUpdateValue: 1 }
+    const backstageValues = (() => {
 
-          const qualityUpdateValue =
-          item.sell_in < 0 ? -item.quality
-          : item.sell_in > 10 ? 1
-          : item.sell_in > 6 ? 2
-          : 3;
+      const qualityUpdateValue =
+      item.sell_in < 0 ? -item.quality
+      : item.sell_in > 10 ? 1
+      : item.sell_in > 6 ? 2
+      : 3;
 
-          return { qualityUpdateValue }
-        }
-      : () => {
-        const qualityUpdateValue =
-          item.sell_in === 0 ? -2 : -1;
+      return { qualityUpdateValue }
+    })()
+    const itemValues = (() => {
+      const qualityUpdateValue =
+        item.sell_in === 0 ? -2 : -1;
 
-        return { qualityUpdateValue }
-      }
+      return { qualityUpdateValue }
+    })()
 
-    return updateValuesFn();
+    const updateValues =
+      sulfurasRegex.test(item.name) ? sulfurasValues
+      : conjuredRegex.test(item.name) ? conjuredValues
+      : item.name === AGED_BRIE ? ageBrieValues
+      : backstageRegex.test(item.name) ? backstageValues
+      : itemValues
+
+    return updateValues;
   }
 
   const setQuailty = (item, upgradeValues) => {
